b15cff1674196bdf5bc5699a57b744e1e8e5d1f2
Rajdeep Singh <rajdeepsingh141204@gmail.com>
2024-05-25 01:57:00 +0530

    add implementation

--- dif.java
+++ dif.java
@@ -2,7 +2,7 @@ public class dif {
     public static void main(String[] args) {
         int  a = 4;
         int b = 7;
-        System.out.print("sum is"+(a-b));
+        System.out.print("diff is is"+(a-b));
     
     }
 }
